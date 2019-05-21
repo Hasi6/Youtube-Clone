1462f43a04fc6be094181660c375eb1a73a0a225
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-21 13:43:42 +0530

    Create header and side navbar

---
+++ includes/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HasiTube</title>
+
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <!-- Font Awesome -->
+    
+
+    <!-- Google jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Bootstrap js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <!-- js -->
+    <script src="./assets/js/commonActions.js"></script>
+</head>
+<body>
+
+    <div id="pageContainer">
+        <div id="mastHeadContainer">
+        <button class="navShowHide" type="submit"><img src="./assets/images/icons/menu.png" alt=""></button>
+
+            <div class="searchBarContainer">
+                <form action="search.php" method="GET">
+                    <input type="search" class="searchBar" name="term" placeholder="Search...">
+                    <button class="searchButton">
+                        <img src="./assets/images/icons/search.png" alt="">
+                    </button>
+                </form>
+            </div>
+
+            <div class="uploadProfile">
+                <a href="upload.php">
+                    <img src="./assets/images/icons/upload.png" alt="uoload image" class="upload">
+                </a>
+                <a href="#">
+                    <img src="./assets/images/icons/profile.png" alt="profile pic">
+                </a>
+            </div>
+
+        </div>
+
+        <div id="sideNavContainer" style="display: none">
+            
+        </div>
+
+        <div id="mainSectionContainer">
+            <div id="mainContentContainer">
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    require_once('./includes/header.php');
+?>
+                Hello Hasi
+<?php
+    require_once('./includes/footer.php');
+?>
