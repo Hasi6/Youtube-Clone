f1462490e8df29e4926720e776fb7c79469a3ee9
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-22 18:22:24 +0530

    Create VideoProcessor.php and VideoUploadData.php

--- processing.php
+++ processing.php
@@ -1,6 +1,8 @@
 <!-- Requied Files -->
 <?php
     require_once('./includes/header.php');
+    require_once('./includes/classes/VideoUploadData.php');
+    require_once('./includes/classes/VideoProcessor.php');
 ?>
 
 <!-- IF Files not not send to the pages show the message -->
@@ -14,7 +16,23 @@
 
     if(!isset($_POST["uploadButton"])){
         echo  $message;
+        exit();
     }
 
-    
+    //Set video details to variables
+    $fileInput = $_POST["fileInput"];
+    $titleInput = $_POST["titleInput"];
+    $descriptionInput = $_POST["descriptionInput"];
+    $privacyInput = $_POST["privacyInput"];
+    $categoryInput = $_POST["categoryInput"];
+    $uploadedBy = "Hasi";
+
+    // Send upload file data to the VideoUploadData.php 
+    $videoUploadDate = new VideoUploadData($fileInput, $titleInput, $descriptionInput, $privacyInput, $categoryInput, $uploadedBy);
+
+    // Upload video
+    $videoProcessor = new VideoProcessor($con);
+    $wasSuccess = $videoProcessor->upload($videoUploadDate);
+
+
 ?>
