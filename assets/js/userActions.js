fd1ae4cb38c6c688710ecaba912018c365dbf814
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-05-27 22:54:11 +0530

    Update
    now when User Press Subscribe Button Live update the Subscribers count and the color of the subscribe button

--- assets/js/userActions.js
+++ assets/js/userActions.js
@@ -5,8 +5,20 @@ function subscribe(userTo, userFrom, button) {
         return;
     }
 
-    $.post("ajax/subscribe.php")
-        .done(function() {
-            console.log("done");
+    $.post("Ajax/subscribe.php", { userTo: userTo, userFrom: userFrom })
+        .done(function(count) {
+
+            if (count != null) {
+                $(button).toggleClass("subscribe unsubscribe");
+
+                if ($(button).hasClass("subscribe")) {
+                    var buttonText = "SUBSCRIBE";
+                } else {
+                    var buttonText = "SUBSCRIBED";
+                }
+                $(button).text(buttonText + " " + count);
+            } else {
+                alert("Some thing went Wrong");
+            }
         });
 }
