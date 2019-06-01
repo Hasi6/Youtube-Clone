7772972e898faf25fe91e594d298637f9be83277
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-06-01 20:09:23 +0530

    Update Subscriptionprovider.php

--- includes/classes/Subscriptionprovider.php
+++ includes/classes/Subscriptionprovider.php
@@ -15,7 +15,43 @@
             $subscriptions = $this->userLoggedInObj->getSubscriptions();
 
             if(sizeof($subscriptions) > 0 ){
-                
+                /* If we subscribe 3 users as a example user1, user2, user3 we have to do some thing like this to get their 
+                videos to display in home page we have to create query like this
+                SELECT * FROM videos WHERE uploadedby= ? OR uploadedby= ? OR uploadedby= ?*/
+                // $query->bindParam(1, "user1");
+                // $query->bindParam(2, "user2");
+                // $query->bindParam(3, "user3");
+
+                $condition = "";
+                $i = 0;
+
+                while($i < sizeof($subscriptions)){
+                    if($i == 0){
+                        $condition .= "WHERE uploadedBy=?";
+                    } else{
+                        $condition .= " OR uploadedBy=?";
+
+                    }
+                    $i++;
+                }
+
+                $videoSql = "SELECT * FROM videos $condition ORDER BY uploadDate DESC";
+                $videoQuery = $this->con->prepare($videoSql);
+
+                $i = 1;
+
+                foreach($subscriptions as $sub){
+                    $videoQuery->bindParam($i, $subUsername);
+                    $subUsername = $sub->getUsername();
+                    $i++;
+                }
+                $videoQuery->execute();
+
+                while($row = $videoQuery->fetch(PDO::FETCH_ASSOC)){
+                    $video = new Video($this->con, $row, $this->userLoggedInObj);
+                    array_push($videos, $video);
+                }
+
             }
             return $videos;
         }
