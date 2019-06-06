4918bd65a9bb38536121343a726896c8a9953a02
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2019-06-06 21:06:40 +0530

    update
    Write query to get thumbnails from database and display them in the video edit page

--- editVideo.php
+++ editVideo.php
@@ -3,6 +3,7 @@
     require_once("./includes/classes/VideoPlayer.php");
     require_once("./includes/classes/videoDetails.php");
     require_once("./includes/classes/VideoUploadData.php");
+    require_once("./includes/classes/SelectThumbnail.php");
 ?>
 
 <?php
@@ -13,6 +14,7 @@
 
     if(!isset($_GET["videoId"])){
         echo "No Video Selected to Edit";
+        exit();
     }
 
     $video = new Video($con, $_GET["videoId"], $userLoggedInObj);
@@ -22,6 +24,26 @@
         exit();
     }
 ?>
+
+<div class="editVideoContainer column">
+
+    <div class="topSection">
+
+        <?php
+            $videoPlayer = new VideoPlayer($video);
+            echo $videoPlayer->create(false);
+
+            $selectThumbnail = new SelectThumbnail($con, $video);
+            echo $selectThumbnail->create();
+        ?>
+
+    </div>
+
+    <div class="bottomSection">
+        
+    </div>
+
+</div>
 
 
 <?php
